4d7e1b819f3aa8c9f2e01ad2e174a759a2dd46ab
Ďodin <jozefknut@gmail.com>
2026-05-28 11:25:01 +0200

    orders

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (auth()->check()) {
-        return redirect()->route('prehlad');
+        return redirect()->route('zakazky');
     }
     return view('welcome');
 });
